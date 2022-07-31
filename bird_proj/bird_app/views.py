98c25ed4d886dd5d55f25ba1d9864f5dbe569ef7
John-OK <john.ok.esp@gmail.com>
2022-07-31 12:09:01 -0500

    Linked Django with React, migrated models and added views for user_auth

--- bird_proj/bird_app/views.py
+++ bird_proj/bird_app/views.py
@@ -1,7 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.contrib.auth import authenticate, login, logout
+from django.core import serializers
+from rest_framework.decorators import api_view
+from django.views.decorators.csrf import csrf_exempt
+# from .models import Preference
 
 def send_the_homepage(request):
     print('home')
-    
-    return HttpResponse("hello")
+    theIndex = open('static/index.html').read()
+    return HttpResponse(theIndex)
+
+@api_view(['POST'])
+def sign_up(request):
+    try:
+        User.objects.create_user(username=request.data['email'], password=request.data['password'], email=request.data['email'])
+    except Exception as e:
+        print(str(e))
+    return HttpResponse('hi')
+
+@api_view(['POST'])
+def log_in(request):
+    print(dir(request))
+    print(dir(request._request))
+
+    # DRF assumes that the body is JSON, and automatically parses it into a dictionary at request.data
+    email = request.data['email']
+    password = request.data['password']
+    # user = authenticate(username=email, password=password, email=email)
+    user = authenticate(username=email, password=password)
+    print('user?')
+    print(user.email)
+    print(user.password)
+    if user is not None:
+        if user.is_active:
+            try:
+                # access the base request, not the DRF request
+                # this starts a login session for this user
+                login(request._request, user)
+            except Exception as e:
+                print('except')
+                print(str(e))
+            return HttpResponse('success!')
+            # Redirect to a success page.
+        else:
+            return HttpResponse('not active!')
+            # Return a 'disabled account' error message
+    else:
+        return HttpResponse('no user!')
+        # Return an 'invalid login' error message.
+
+@api_view(['POST'])
+def log_out(request):
+    logout(request)
+    return JsonResponse({'success':True})
+
+@api_view(['GET'])
+def who_am_i(request):
+    # Make sure that you don't send sensitive information to the client, such as password hashes
+    # raise Exception('oops')
+    if request.user.is_authenticated:
+        data = serializers.serialize("json", [request.user], fields=['email', 'username'])
+
+        return HttpResponse(data)
+    else:
+        return JsonResponse({'user':None})
--- bird_proj/bird_proj/settings.py
+++ bird_proj/bird_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+# import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,10 +77,12 @@ WSGI_APPLICATION = 'bird_proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'assessment_five',
     }
 }
+
+AUTH_USER_MODEL = 'bird_app.AppUser'
 
 
 # Password validation
@@ -117,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
