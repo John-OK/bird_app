c2d76d54f0b7c3c73cc56ec16e4dae9739a3c439
John-OK <john.ok.esp@gmail.com>
2022-07-31 19:16:54 -0500

    Added README, basic user signup, login, logout added.

---
+++ backend/bird_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.send_the_homepage),
+    path('signup', views.sign_up),
+    path('login', views.log_in),
+    path('logout', views.log_out),
+    path('whoami', views.who_am_i)
+]
