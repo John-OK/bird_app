424fe0402db3e002101a3658b9e4a5f2458181ae
John O'Keeffe <john.ok.dev@gmail.com>
2023-11-10 21:37:03 +0100

    Add missing HttpRequest import in views.py

--- backend/bird_app/views.py
+++ backend/bird_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpRequest
 from django.contrib.auth import authenticate, login, logout
 from django.core import serializers
 from rest_framework.decorators import api_view
