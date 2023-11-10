4faf86ec0d70ff48f37752fb3a6b2701bef23863
John O'Keeffe <john.ok.dev@gmail.com>
2023-11-10 14:33:40 +0100

    Change name of database.

--- backend/bird_proj/settings.py
+++ backend/bird_proj/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'bird_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bird_confirm',
+        'NAME': 'hellobirdie',
     }
 }
 
