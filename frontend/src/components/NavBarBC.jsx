6bec5a9812ac413436cb8364f5e5b8723d1e5e7e
John O'Keeffe <john.ok.dev@gmail.com>
2023-11-15 22:00:08 +0100

    Add indication that data is being fetched during xeno-canto API call.

--- frontend/src/components/NavBarBC.jsx
+++ frontend/src/components/NavBarBC.jsx
@@ -10,6 +10,7 @@ function NavBarBC(props) {
   const [position, setPosition] = useState(null)
   const [birdData, setBirdData] = useState(null)
   const [boxLimits, setBoxLimits] = useState(null)
+  const [loading, setLoading] = useState(false)
 
   useEffect( ()=> {
       props.whoAmI()
@@ -60,6 +61,7 @@ function NavBarBC(props) {
 
   const checkBird = (event) => {
     event.preventDefault();
+    setLoading(true);
     let birdName = document.getElementById('name').value
     // Handle no user input for birdName. Will get all birds in radius around user
     if (birdName === "") {
@@ -73,7 +75,8 @@ function NavBarBC(props) {
           const nearbyBirds = response.data.filtered_data.filtered_birds
           setBirdData(nearbyBirds)
           setBoxLimits(response.data.box_limits)
-        })
+          setLoading(false);
+        });
 }
 
   return (
@@ -91,7 +94,9 @@ function NavBarBC(props) {
               <label>
                   <input id='name' type="text" placeholder="Name (blank for all birds)" />
               </label>
-              <button type="submit">Check</button>
+              <button type="submit">
+                {loading ? "Loading birds..." : "Check for birds" }
+              </button>
               </form>
             </div>
 
